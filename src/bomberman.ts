c8d8b7dd7908c0f0e06973a5a5d7f169cea14538
Jesús Silva Méndez <jesussilva@Jesus-Silva.local>
2021-12-28 21:07:32 +0100

    feat (game & actors): se ha añadido movimiento a los actores, y se ha acotado el movimiento a los bordes del mapa

--- src/bomberman.ts
+++ src/bomberman.ts
@@ -5,41 +5,45 @@ import { Drawer } from './drawer'
 import { BombermanKey, KeyboardMap } from './utils/keyboard-map'
 
 export class Bomberman extends Actor {
-  origin: Position
+  position: Position
   keyboardMap: KeyboardMap
   size: Position = { x: 50, y: 50 }
+  speed: Position = { x: 50, y: 50 }
 
   constructor(position: Position, keyboardMap: KeyboardMap) {
     super(position)
-    this.origin = { x: position.x, y: position.y }
+    this.position = position
     this.keyboardMap = keyboardMap
+    console.log(this.keyboardMap)
   }
 
   keyboard_event_down(key: string) {
+    console.log(key)
     const tecla = this.keyboardMap[key]
 
-    if (tecla == BombermanKey.LEFT) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
-    } else if (tecla == BombermanKey.RIGHT) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
-    } else if (tecla == BombermanKey.UP) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
-    } else if (tecla == BombermanKey.DOWN) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
+    if (tecla == BombermanKey.LEFT && this.position.x > 50) {
+      this.position.x -= 50
+    } else if (tecla == BombermanKey.RIGHT && this.position.x < 1050) {
+      this.position.x += 50
+    } else if (tecla == BombermanKey.UP && this.position.y > 50) {
+      this.position.y -= 50
+    } else if (tecla == BombermanKey.DOWN && this.position.y < 650) {
+      this.position.y += 50
     }
+
+    console.log(this.position.x)
   }
 
   keyboard_event_up(key: string) {
     const tecla = this.keyboardMap[key]
 
-    if (tecla == BombermanKey.LEFT) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
-    } else if (tecla == BombermanKey.RIGHT) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
-    } else if (tecla == BombermanKey.UP) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
-    } else if (tecla == BombermanKey.DOWN) {
-      console.log({ ...this.keyboardMap, Tecla: tecla })
+    if (
+      tecla === BombermanKey.LEFT ||
+      tecla === BombermanKey.RIGHT ||
+      tecla === BombermanKey.UP ||
+      tecla === BombermanKey.DOWN
+    ) {
+      this.speed = { x: 0, y: 0 }
     }
   }
 
@@ -48,5 +52,16 @@ export class Bomberman extends Actor {
     drawer.draw()
   }
 
-  update(delta: number) {}
+  update(delta: number) {
+    // if (this.position.x > 1100) {
+    //   this.position.x -= 50
+    // } else if (this.position.x < 50) {
+    //   this.position.x += 50
+    // }
+    // if (this.position.y > 700) {
+    //   this.position.y -= 50
+    // } else if (this.position.y < 50) {
+    //   this.position.y += 50
+    // }
+  }
 }
