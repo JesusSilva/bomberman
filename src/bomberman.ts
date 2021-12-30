9336b9e8dba304b0d6639d0065a0ad15a96fb9c1
Jesús Silva Méndez <jesussilva@Jesus-Silva.local>
2021-12-30 13:06:25 +0100

    feat (game & assets): se ha corregido la velocidad del movimiento y se han añadido texturas

--- src/bomberman.ts
+++ src/bomberman.ts
@@ -1,53 +1,186 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Actor } from './classes/Actor'
 import { Position } from './classes/Position'
-import { Drawer } from './drawer'
-import { BombermanKey, KeyboardMap } from './utils/keyboard-map'
+import { KeyboardMap } from './utils/keyboard-map'
 
+const bomberman_white_front_1 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_front/bomberman_1.png')
+const bomberman_white_front_2 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_front/bomberman_2.png')
+const bomberman_white_front_3 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_front/bomberman_3.png')
+const bomberman_white_right_1 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_right/bomberman_1.png')
+const bomberman_white_right_2 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_right/bomberman_2.png')
+const bomberman_white_right_3 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_right/bomberman_3.png')
+const bomberman_white_back_1 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_back/bomberman_1.png')
+const bomberman_white_back_2 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_back/bomberman_2.png')
+const bomberman_white_back_3 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_back/bomberman_3.png')
+const bomberman_white_left_1 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_left/bomberman_1.png')
+const bomberman_white_left_2 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_left/bomberman_2.png')
+const bomberman_white_left_3 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_left/bomberman_3.png')
+const bomberman_white_dead_1 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_dead/bomberman_1.png')
+const bomberman_white_dead_2 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_dead/bomberman_2.png')
+const bomberman_white_dead_3 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_dead/bomberman_3.png')
+const bomberman_white_dead_4 = require('../public/images/sprites/bombermans/bomberman_white/bomberman_dead/bomberman_4.png')
+
+const bomberman_blue_front_1 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_front/bomberman_1.png')
+const bomberman_blue_front_2 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_front/bomberman_2.png')
+const bomberman_blue_front_3 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_front/bomberman_3.png')
+const bomberman_blue_right_1 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_right/bomberman_1.png')
+const bomberman_blue_right_2 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_right/bomberman_2.png')
+const bomberman_blue_right_3 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_right/bomberman_3.png')
+const bomberman_blue_back_1 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_back/bomberman_1.png')
+const bomberman_blue_back_2 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_back/bomberman_2.png')
+const bomberman_blue_back_3 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_back/bomberman_3.png')
+const bomberman_blue_left_1 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_left/bomberman_1.png')
+const bomberman_blue_left_2 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_left/bomberman_2.png')
+const bomberman_blue_left_3 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_left/bomberman_3.png')
+const bomberman_blue_dead_1 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_dead/bomberman_1.png')
+const bomberman_blue_dead_2 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_dead/bomberman_2.png')
+const bomberman_blue_dead_3 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_dead/bomberman_3.png')
+const bomberman_blue_dead_4 = require('../public/images/sprites/bombermans/bomberman_blue/bomberman_dead/bomberman_4.png')
 export class Bomberman extends Actor {
   position: Position
   keyboardMap: KeyboardMap
+  playerNumber: number
   size: Position = { x: 50, y: 50 }
   speed: Position = { x: 50, y: 50 }
   keystrokes: any = {}
   lastKeyPress: string | null = null
+  bomberman: HTMLImageElement = new Image()
+  animationIndex = 1
+  lastDirection!: string
+  pjsDrawed = false
 
-  constructor(position: Position, keyboardMap: KeyboardMap) {
+  assets: { [key: number]: { [key: string]: { [key: number]: any } } } = {
+    1: {
+      front: {
+        1: bomberman_white_front_1,
+        2: bomberman_white_front_2,
+        3: bomberman_white_front_3
+      },
+      back: {
+        1: bomberman_white_back_1,
+        2: bomberman_white_back_2,
+        3: bomberman_white_back_3
+      },
+      right: {
+        1: bomberman_white_right_1,
+        2: bomberman_white_right_2,
+        3: bomberman_white_right_3
+      },
+      left: {
+        1: bomberman_white_left_1,
+        2: bomberman_white_left_2,
+        3: bomberman_white_left_3
+      },
+      dead: {
+        1: bomberman_white_dead_1,
+        2: bomberman_white_dead_2,
+        3: bomberman_white_dead_3,
+        4: bomberman_white_dead_4
+      }
+    },
+    2: {
+      front: {
+        1: bomberman_blue_front_1,
+        2: bomberman_blue_front_2,
+        3: bomberman_blue_front_3
+      },
+      back: {
+        1: bomberman_blue_back_1,
+        2: bomberman_blue_back_2,
+        3: bomberman_blue_back_3
+      },
+      right: {
+        1: bomberman_blue_right_1,
+        2: bomberman_blue_right_2,
+        3: bomberman_blue_right_3
+      },
+      left: {
+        1: bomberman_blue_left_1,
+        2: bomberman_blue_left_2,
+        3: bomberman_blue_left_3
+      },
+      dead: {
+        1: bomberman_blue_dead_1,
+        2: bomberman_blue_dead_2,
+        3: bomberman_blue_dead_3,
+        4: bomberman_blue_dead_4
+      }
+    }
+  }
+
+  constructor(position: Position, keyboardMap: KeyboardMap, playerNumber: number) {
     super(position)
     this.position = position
     this.keyboardMap = keyboardMap
+    this.playerNumber = playerNumber
     this.formatKeystrokes()
   }
 
   draw(delta: number, ctx: CanvasRenderingContext2D) {
-    const drawer = new Drawer(ctx, this.position, this.size, 'rect', 'yellow')
-    drawer.draw()
+    if (!this.pjsDrawed) {
+      this.playerNumber === 1
+        ? this.changePjImage(ctx, 'right')
+        : this.playerNumber === 2
+        ? this.changePjImage(ctx, 'left')
+        : ''
+      this.pjsDrawed = true
+    } else {
+      this.bomberman.src = this.assets[this.playerNumber][this.lastDirection][1]
+      ctx.drawImage(this.bomberman, this.position.x, this.position.y, 40, 55)
+    }
   }
 
-  // eslint-disable-next-line complexity
-  update(delta: number) {
-    if (
-      this.position.x < 1050 &&
-      (this.keystrokes['d'] || this.keystrokes['ArrowRight'])
-    ) {
-      this.position.x += 50
-    } else if (
-      this.position.x > 50 &&
-      (this.keystrokes['a'] || this.keystrokes['ArrowLeft'])
-    ) {
-      this.position.x -= 50
+  update(delta: number, ctx: CanvasRenderingContext2D) {
+    if (this.position.x < 1050 && (this.keystrokes['d'] || this.keystrokes['ArrowRight'])) {
+      this.position.x += 5
+      this.changePjImage(ctx, 'right')
+    } else if (this.position.x > 50 && (this.keystrokes['a'] || this.keystrokes['ArrowLeft'])) {
+      this.position.x -= 5
+      this.changePjImage(ctx, 'left')
     }
 
-    if (
-      this.position.y < 650 &&
-      (this.keystrokes['s'] || this.keystrokes['ArrowDown'])
-    ) {
-      this.position.y += 50
-    } else if (
-      this.position.y > 50 &&
-      (this.keystrokes['w'] || this.keystrokes['ArrowUp'])
-    ) {
-      this.position.y -= 50
+    if (this.position.y < 650 && (this.keystrokes['s'] || this.keystrokes['ArrowDown'])) {
+      this.position.y += 5
+      this.changePjImage(ctx, 'front')
+    } else if (this.position.y > 50 && (this.keystrokes['w'] || this.keystrokes['ArrowUp'])) {
+      this.position.y -= 5
+      this.changePjImage(ctx, 'back')
+    }
+  }
+
+  changePjImage(ctx: CanvasRenderingContext2D, direction: string): void {
+    switch (direction) {
+      case 'back':
+        this.lastDirection = 'back'
+        this.bomberman.src = this.assets[this.playerNumber][this.lastDirection][this.animationIndex]
+        ctx.drawImage(this.bomberman, this.position.x, this.position.y, 40, 55)
+        break
+      case 'front':
+        this.lastDirection = 'front'
+        this.bomberman.src = this.assets[this.playerNumber][this.lastDirection][this.animationIndex]
+        ctx.drawImage(this.bomberman, this.position.x, this.position.y, 40, 55)
+        break
+      case 'right':
+        this.lastDirection = 'right'
+        this.bomberman.src = this.assets[this.playerNumber][this.lastDirection][this.animationIndex]
+        ctx.drawImage(this.bomberman, this.position.x, this.position.y, 40, 55)
+        break
+      case 'left':
+        this.lastDirection = 'left'
+        this.bomberman.src = this.assets[this.playerNumber][this.lastDirection][this.animationIndex]
+        ctx.drawImage(this.bomberman, this.position.x, this.position.y, 40, 55)
+        break
+      default:
+        break
+    }
+    this.animationControl()
+  }
+
+  animationControl() {
+    if (this.animationIndex <= 2) {
+      this.animationIndex++
+    } else {
+      this.animationIndex = 1
     }
   }
 
@@ -59,38 +192,17 @@ export class Bomberman extends Actor {
     }
   }
 
-  keyboard_event_down(key: string) {
-    const tecla = this.keyboardMap[key]
-
-    if (Object.keys(this.keyboardMap).includes(key)) {
-      this.handleKeyboardEvents(key, null)
+  keyboard_event_down(event: KeyboardEvent) {
+    if (Object.keys(this.keyboardMap).includes(event.key)) {
+      this.handleKeyboardEvents(event.key, null)
     }
 
-    if (tecla === BombermanKey.LEFT && this.position.x > 50) {
-      this.position.x -= 50
-    } else if (tecla === BombermanKey.RIGHT && this.position.x < 1050) {
-      this.position.x += 50
-    } else if (tecla === BombermanKey.UP && this.position.y > 50) {
-      this.position.y -= 50
-    } else if (tecla === BombermanKey.DOWN && this.position.y < 650) {
-      this.position.y += 50
-    }
+    console.log(this.position.x)
   }
 
   keyboard_event_up(key: string) {
-    const tecla = this.keyboardMap[key]
-
     if (Object.keys(this.keyboardMap).includes(key)) {
       this.handleKeyboardEvents(null, key)
-    }
-
-    if (
-      tecla === BombermanKey.LEFT ||
-      tecla === BombermanKey.RIGHT ||
-      tecla === BombermanKey.UP ||
-      tecla === BombermanKey.DOWN
-    ) {
-      this.speed = { x: 0, y: 0 }
     }
   }
 
@@ -107,7 +219,5 @@ export class Bomberman extends Actor {
     if (keyup) {
       this.keystrokes[keyup] = false
     }
-
-    console.log(this.keystrokes)
   }
 }
--- src/map.ts
+++ src/map.ts
@@ -2,6 +2,18 @@
 import { Drawer } from './drawer'
 import { Position } from './classes/Position'
 
+const lvl1_wall = require('../public/images/sprites/levels/level_1/wall.png')
+const lvl1_grass = require('../public/images/sprites/levels/level_1/grass.png')
+const lvl1_obstacle = require('../public/images/sprites/levels/level_1/obstacle.png')
+const lvl1_barrier = require('../public/images/sprites/levels/level_1/barrier.png')
+
+const lvl2_wall = require('../public/images/sprites/levels/level_2/wall.png')
+const lvl2_grass = require('../public/images/sprites/levels/level_2/grass.png')
+const lvl2_obstacle = require('../public/images/sprites/levels/level_2/obstacle.png')
+const lvl2_barrier = require('../public/images/sprites/levels/level_2/barrier.png')
+
+const finish = require('../public/images/sprites/finish.png')
+
 export class Map {
   position!: Position
   level: number
@@ -14,7 +26,7 @@ export class Map {
       [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
       [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
       [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
       [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
@@ -28,9 +40,41 @@ export class Map {
   widthOfElement = 50
   heightOfElement = 50
 
+  wall: HTMLImageElement
+  grass: HTMLImageElement
+  barrier: HTMLImageElement
+  obstacle: HTMLImageElement
+  finish: HTMLImageElement
+
   constructor(level: number, position = { x: 0, y: 0 }) {
     this.position = position
     this.level = level
+    this.wall = new Image()
+    this.grass = new Image()
+    this.barrier = new Image()
+    this.obstacle = new Image()
+    this.finish = new Image()
+    this.loadAssets()
+  }
+
+  loadAssets() {
+    switch (this.level) {
+      case 1:
+        this.wall.src = lvl1_wall
+        this.grass.src = lvl1_grass
+        this.barrier.src = lvl1_barrier
+        this.obstacle.src = lvl1_obstacle
+        break
+      case 2:
+        this.wall.src = lvl2_wall
+        this.grass.src = lvl2_grass
+        this.barrier.src = lvl2_obstacle
+        this.obstacle.src = lvl2_barrier
+        break
+      default:
+        break
+    }
+    this.finish.src = finish
   }
 
   draw(delta: number, ctx: CanvasRenderingContext2D) {
@@ -50,18 +94,27 @@ export class Map {
           case 0:
             drawer = new Drawer(ctx, position, size, 'rect', 'blue')
             drawer.draw()
+            ctx.drawImage(this.grass, position.x, position.y, 50, 50)
             break
           case 1:
             drawer = new Drawer(ctx, position, size, 'rect', 'green')
             drawer.draw()
+            ctx.drawImage(this.wall, position.x, position.y, 50, 50)
             break
           case 2:
             drawer = new Drawer(ctx, position, size, 'rect', 'red')
             drawer.draw()
+            ctx.drawImage(this.obstacle, position.x, position.y, 50, 50)
             break
           case 3:
             drawer = new Drawer(ctx, position, size, 'rect', 'black')
             drawer.draw()
+            ctx.drawImage(this.barrier, position.x, position.y, 50, 50)
+            break
+          case 4:
+            drawer = new Drawer(ctx, position, size, 'rect', 'pink')
+            drawer.draw()
+            ctx.drawImage(this.finish, position.x, position.y, 50, 50)
             break
           default:
             break
@@ -70,5 +123,5 @@ export class Map {
     })
   }
 
-  update(delta: number) {}
+  update(delta: number, ctx: CanvasRenderingContext2D) {}
 }
